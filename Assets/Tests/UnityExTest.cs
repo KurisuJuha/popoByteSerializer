cf03b838566f7aa2f26e0f2aedbc14ea20c90232
KYapp <kukiyomenaisapp@gmail.com>
2023-02-13 23:13:43 +0900

    Vector2

--- Assets/Tests/UnityExTest.cs
+++ Assets/Tests/UnityExTest.cs
@@ -4,4 +4,15 @@ using NUnit.Framework;
 
 public class UnityExTest
 {
+    [Test]
+    public void Vector2Test()
+    {
+        var value = new Vector2(2.4f, 3234.4f);
+        DataWriter writer = new DataWriter();
+        writer.Puts(value);
+        DataReader reader = new DataReader(writer.bytes);
+        var readValue = reader.ReadVector2();
+        Assert.AreEqual(value, readValue);
+    }
+
 }

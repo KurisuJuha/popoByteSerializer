569f8704206fc5d8765a967038a47a818b1a95d3
KYapp <kukiyomenaisapp@gmail.com>
2023-02-13 22:14:48 +0900

    Vector2Int

--- Assets/popoByteSerializer/Runtime/DataReaderUnityEx.cs
+++ Assets/popoByteSerializer/Runtime/DataReaderUnityEx.cs
@@ -7,5 +7,8 @@ namespace JuhaKurisu.PopoTools.ByteSerializer
         public static Vector2 ReadVector2(this DataReader self)
             => new Vector2(self.ReadFloat(), self.ReadFloat());
 
+        public static Vector2Int ReadVector2Int(this DataReader self)
+            => new Vector2Int(self.ReadInt(), self.ReadInt());
+
     }
 }
--- Assets/popoByteSerializer/Runtime/DataWriterUnityEx.cs
+++ Assets/popoByteSerializer/Runtime/DataWriterUnityEx.cs
@@ -7,5 +7,8 @@ namespace JuhaKurisu.PopoTools.ByteSerializer
         public static DataWriter Puts(this DataWriter self, Vector2 value)
             => self.Puts(value.x).Puts(value.y);
 
+        public static DataWriter Puts(this DataWriter self, Vector2Int value)
+            => self.Puts(value.x).Puts(value.y);
+
     }
 }

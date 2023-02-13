a20442b1ec4edf9f5f9a6a10b698f52372c5084e
KYapp <kukiyomenaisapp@gmail.com>
2023-02-13 23:12:22 +0900

    Boolean

--- Assets/Tests/DefaultTest.cs
+++ Assets/Tests/DefaultTest.cs
@@ -3,4 +3,14 @@ using NUnit.Framework;
 
 public class DefaultTest
 {
+    [Test]
+    public void BooleanTest()
+    {
+        var value = false;
+        DataWriter writer = new DataWriter();
+        writer.Puts(value);
+        DataReader reader = new DataReader(writer.bytes);
+        var readValue = reader.ReadBoolean();
+        Assert.AreEqual(value, readValue);
+    }
 }
